442489f5635863bfcaf5ddbf4922e70b3683ef20
Juliyapz <kremleva_yuliya@mail.ru>
2021-07-27 18:12:51 +0900

    create cleaning section

--- js/main.js
+++ js/main.js
@@ -48,6 +48,13 @@ $(document).ready(function () {
     $('body,html').animate({ scrollTop: top }, 1000);
   });
 
+  $("#cln").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
+
   $("#nav").on("click", "a", function (event) {
     event.preventDefault();
     var id = $(this).attr('href'),
